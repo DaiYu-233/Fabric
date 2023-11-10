202adfb5173aa120c676111059af3e821d66dcb6
DaiYu <840673183@qq.com>
2023-11-10 22:12:13 +0800

    Change Main

---
+++ src/main/java/fun/daiyu/Main.java
@@ -0,0 +1,16 @@
+package fun.daiyu;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
