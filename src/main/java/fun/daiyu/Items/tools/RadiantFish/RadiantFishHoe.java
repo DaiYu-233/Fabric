4a6d043c8a5ad03c034066cd7386e094be6c3de4
DaiYu <840673183@qq.com>
2023-11-11 13:20:57 +0800

    Update

---
+++ src/main/java/fun/daiyu/Items/tools/RadiantFish/RadiantFishHoe.java
@@ -0,0 +1,11 @@
+package fun.daiyu.Items.tools.RadiantFish;
+
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ToolMaterial;
+
+public class RadiantFishHoe extends HoeItem {
+    public RadiantFishHoe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
---
+++ src/main/java/fun/daiyu/Items/tools/RadiantFish/RadiantFishShovel.java
@@ -0,0 +1,11 @@
+package fun.daiyu.Items.tools.RadiantFish;
+
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ToolMaterial;
+
+public class RadiantFishShovel extends ShovelItem {
+    public RadiantFishShovel(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
+    }
+}
