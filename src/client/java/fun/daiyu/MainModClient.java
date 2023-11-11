c2ccfb05e47a8277eb9fbfce2e054fac93ba8f0a
DaiYu <840673183@qq.com>
2023-11-11 15:44:18 +0800

    Update

--- src/client/java/fun/daiyu/MainModClient.java
+++ src/client/java/fun/daiyu/MainModClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package fun.daiyu;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class MainModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
