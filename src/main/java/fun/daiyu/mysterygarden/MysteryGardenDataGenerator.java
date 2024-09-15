4bc5c6dc50e333b0ec2b6e24cb5e1920ea28149e
DaiYu-233 <840673183@qq.com>
2024-09-15 20:08:44 +0800

    初次提交

---
+++ src/main/java/fun/daiyu/mysterygarden/MysteryGardenDataGenerator.java
@@ -0,0 +1,11 @@
+package fun.daiyu.mysterygarden;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MysteryGardenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
